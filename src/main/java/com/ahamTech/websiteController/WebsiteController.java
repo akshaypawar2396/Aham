a9747d7b2bf88bff246caa8280c7acf43fec08ac
Akshay Pawar <akshaypawar.5146@gmail.com>
2021-12-15 20:58:34 +0530

    first commit

---
+++ src/main/java/com/ahamTech/websiteController/WebsiteController.java
@@ -0,0 +1,16 @@
+package com.ahamTech.websiteController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebsiteController {
+	
+	@RequestMapping("/")
+	public String fetchHomePage()
+	{
+		return "website/index";
+		
+	}
+
+}
---
+++ src/test/java/com/ahamTech/AhamTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahamTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhamTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
